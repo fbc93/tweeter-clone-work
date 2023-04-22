37e2228eb5f743c2e05ef624f5bcce9631cba34b
yhwa <global user>
2023-04-22 15:10:10 +0900

    Design: Add logo setup basic css
    로고 소재 추가  글로벌 CSS tailwind 테마 요소 경로 alias 셋업

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'Pacifico':['Pacifico']
+      },
+      width:{
+        'container':'360px',
+      },
+      height:{
+        'appbar':'4rem',
+        'navbar':'5rem'
+      },
+      colors:{
+        accentFirst:"rgb(247,223,30)",
+        accentSecond:"rgb(255,69,0)",
+        first:"rgb(20,20,20)",
+        secondary:"rgb(34,34,34)",
+      },
+    },
   },
   plugins: [],
 }
