ca2da8a502cb67e0dbb8a4d1cfbdb705b4b8b5e9
yhwa <hfbc93@gmail.com>
2023-05-02 12:01:09 +0900

    Fix: Add view schema model and fix a tag

--- src/pages/error/wrongpath.tsx
+++ src/pages/error/wrongpath.tsx
@@ -5,8 +5,8 @@ const WrongPath = () => {
     <div className="text-white">
       <h1>잘못된 경로입니다.</h1>
       <p>로그인하거나 회원가입해주세요.</p>
-      <a href="/log-in">로그인</a>
-      <a href="/create-account">회원가입</a>
+      <Link href="/log-in">로그인</Link>
+      <Link href="/create-account">회원가입</Link>
     </div>
   )
 }
