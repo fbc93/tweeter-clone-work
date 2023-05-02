0d8bea78cb77fc5081ecc527804bb13c838edc9a
yhwa <hfbc93@gmail.com>
2023-05-02 15:06:05 +0900

    Feat: Add main page loading skeleton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,25 +42,45 @@ const Home: NextPage = () => {
     >
       <Body>
         <div className="">
-          {data?.posts?.map((post, index) => (
-            <Link
-              href={`/posts/${post.id}`}
-              key={index}
-              className="block border-b border-white/20"
-            >
-              <FeedItem
+          {data?.posts ? (
+            data.posts.map((post, index) => (
+              <Link
+                href={`/posts/${post.id}`}
                 key={index}
-                author={post.author.nickname}
-                avatar={post.author.avatar}
-                content={post.content}
-                image={post?.image}
-                youtube={post?.youtube}
-                likes={post?._count.likes}
-                createdAt={post.createdAt}
-                comments={post?._count.comments}
-              />
-            </Link>
-          ))}
+                className="block border-b border-white/20"
+              >
+                <FeedItem
+                  key={index}
+                  author={post.author.nickname}
+                  avatar={post.author.avatar}
+                  content={post.content}
+                  image={post?.image}
+                  youtube={post?.youtube}
+                  likes={post?._count.likes}
+                  createdAt={post.createdAt}
+                  comments={post?._count.comments}
+                />
+              </Link>
+            ))
+          ) : (
+            <div className="mt-6">
+              <div className="shadow rounded-md p-4 max-w-sm w-full mx-auto">
+                <div className="animate-pulse flex space-x-4">
+                  <div className="rounded-full bg-slate-700 h-10 w-10"></div>
+                  <div className="flex-1 space-y-6 py-1">
+                    <div className="h-2 bg-slate-700 rounded"></div>
+                    <div className="space-y-3">
+                      <div className="grid grid-cols-3 gap-4">
+                        <div className="h-2 bg-slate-700 rounded col-span-2"></div>
+                        <div className="h-2 bg-slate-700 rounded col-span-1"></div>
+                      </div>
+                      <div className="h-2 bg-slate-700 rounded"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </Body>
     </Layout>
